0ef65216cde4b306f96f2b1d13011136077df0be
ixtk <ikhcamot@gmail.com>
2025-11-06 11:03:55 +0400

    Add other jsx variants

---
+++ src/app2.jsx
@@ -0,0 +1,125 @@
+import "./App.css"
+import { Product } from "./Product"
+
+function App2() {
+  return (
+    <div className="container">
+      <div className="products-container">
+        <Product
+          stock={15}
+          salePrice={688}
+          title="Sony WH-1000XM3 Bluetooth Wireless Over Ear Headphones with Mic (Silver)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692947383286-714WUJlhbLS._SL1500_.jpg"
+          price={773}
+        />
+        <Product
+          stock={42}
+          salePrice={55}
+          title="Microsoft Xbox X/S Wireless Controller Robot White"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692255251854-xbox.jpg"
+          price={57}
+        />
+        <Product
+          stock={8}
+          salePrice={null}
+          title="Logitech G733 Lightspeed Wireless Gaming Headset with Suspension Headband, LIGHTSYNC RGB, Blue VO!CE mic Technology and PRO-G Audio Drivers - White"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692257709689-logitech heaphone.jpg"
+          price={384}
+        />
+        <Product
+          stock={23}
+          salePrice={322}
+          title="Sony WH-1000XM5 Wireless Industry Leading Active Noise Cancelling Headphones, 8 Mics for Clear Calling, 30Hr Battery, 3 Min Quick Charge = 3 Hours Playback, Multi Point Connectivity, Alexa-Silver"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692941008275-headphone3.jpg"
+          price={362}
+        />
+        <Product
+          stock={0}
+          salePrice={215}
+          title="Urbanista Los Angeles Sand Gold - World's 1st Solar Powered Hybrid Active Noise Cancelling with Mic Premium Wireless Headphones, Unlimited Playtime"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691056487173-headphon2.jpg"
+          price={265}
+        />
+        <Product
+          stock={156}
+          salePrice={null}
+          title="Xiaomi Wired in-Ear Earphones with Mic, Ultra Deep Bass & Metal Sound Chamber (Blue)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691057474498-earphone.jpg"
+          price={5}
+        />
+        <Product
+          stock={67}
+          salePrice={null}
+          title="boAt Rockerz 370 On Ear Bluetooth Headphones with Upto 12 Hours Playtime, Cozy Padded Earcups and Bluetooth v5.0, with Mic (Buoyant Black)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691057718636-headphone5.jpg"
+          price={12}
+        />
+        <Product
+          stock={31}
+          salePrice={17}
+          title="Amkette EvoFox Elite Ops Wireless Gamepad for Android TV | Google TV | 8+ Hours of Play Time | Zero Lag Connectivity Upto 12 Feets | USB Extender for TV Included | - Dusk Grey"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694100438525-51Prg4Smx-L._SL1500_.jpg"
+          price={18}
+        />
+        <Product
+          stock={12}
+          salePrice={540}
+          title="Samsung Galaxy S22 5G (Phantom White, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691074519203-galaxy S22 5G.jpg"
+          price={760}
+        />
+        <Product
+          stock={5}
+          salePrice={814}
+          title="Samsung Galaxy S22 Ultra 5G (Burgundy, 12GB, 256GB Storage) "
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691074776147-galaxy S22 ultra 5G.jpg"
+          price={1147}
+        />
+        <Product
+          stock={28}
+          salePrice={null}
+          title="Poco by Xiaomi F1 Steel Blue, 6GB RAM, 64GB Storage"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691078463674-poco f1.jpg"
+          price={132}
+        />
+        <Product
+          stock={0}
+          salePrice={166}
+          title="Samsung Galaxy M14 5G (Smoky Teal, 6GB, 128GB Storage)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691075307831-galaxy M14 5G.jpg"
+          price={187}
+        />
+        <Product
+          stock={19}
+          salePrice={128}
+          title="Redmi 80 cm (32 inches) HD Ready Smart LED Fire TV L32R8-FVIN (Black)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694154515879-819Lw2PE8tL._SL1500_.jpg"
+          price={138}
+        />
+        <Product
+          stock={7}
+          salePrice={360}
+          title="Samsung 108 cm (43 inches) Crystal iSmart 4K Ultra HD Smart LED TV UA43CUE60AKLXL (Black)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694154827301-81+JXgPUDLL._SL1500_.jpg"
+          price={396}
+        />
+        <Product
+          stock={34}
+          salePrice={null}
+          title="Mi 125 cm (50 inches) X Series 4K Ultra HD Smart Android LED TV L50M7-A2IN (Black)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694155239807-81UT46-gwRL._SL1500_.jpg"
+          price={384}
+        />
+        <Product
+          stock={3}
+          salePrice={422}
+          title=" Acer 126 cm (50 inches) V Series 4K Ultra HD Smart QLED Google TV AR50GR2851VQD (Black)"
+          image="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694155440745-619--Jabh2L._SL1048_.jpg"
+          price={444}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App2
---
+++ src/app3.jsx
@@ -0,0 +1,214 @@
+import "./App.css"
+
+function App3() {
+  return (
+    <div className="container">
+      <div className="products-container">
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692947383286-714WUJlhbLS._SL1500_.jpg"
+            alt="Sony WH-1000XM3 Bluetooth Wireless Over Ear Headphones with Mic (Silver)"
+          />
+          <h2>
+            Sony WH-1000XM3 Bluetooth Wireless Over Ear Headphones with Mic
+            (Silver)
+          </h2>
+          <p>773 GEL</p>
+          <p>Sale price: 688 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692255251854-xbox.jpg"
+            alt="Microsoft Xbox X/S Wireless Controller Robot White"
+          />
+          <h2>Microsoft Xbox X/S Wireless Controller Robot White</h2>
+          <p>57 GEL</p>
+          <p>Sale price: 55 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692257709689-logitech heaphone.jpg"
+            alt="Logitech G733 Lightspeed Wireless Gaming Headset with Suspension Headband, LIGHTSYNC RGB, Blue VO!CE mic Technology and PRO-G Audio Drivers - White"
+          />
+          <h2>
+            Logitech G733 Lightspeed Wireless Gaming Headset with Suspension
+            Headband, LIGHTSYNC RGB, Blue VO!CE mic Technology and PRO-G Audio
+            Drivers - White
+          </h2>
+          <p>384 GEL</p>
+          <p>Sale price: null GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692941008275-headphone3.jpg"
+            alt="Sony WH-1000XM5 Wireless Industry Leading Active Noise Cancelling Headphones, 8 Mics for Clear Calling, 30Hr Battery, 3 Min Quick Charge = 3 Hours Playback, Multi Point Connectivity, Alexa-Silver"
+          />
+          <h2>
+            Sony WH-1000XM5 Wireless Industry Leading Active Noise Cancelling
+            Headphones, 8 Mics for Clear Calling, 30Hr Battery, 3 Min Quick
+            Charge = 3 Hours Playback, Multi Point Connectivity, Alexa-Silver
+          </h2>
+          <p>362 GEL</p>
+          <p>Sale price: 322 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691056487173-headphon2.jpg"
+            alt="Urbanista Los Angeles Sand Gold - World's 1st Solar Powered Hybrid Active Noise Cancelling with Mic Premium Wireless Headphones, Unlimited Playtime"
+          />
+          <h2>
+            Urbanista Los Angeles Sand Gold - World's 1st Solar Powered Hybrid
+            Active Noise Cancelling with Mic Premium Wireless Headphones,
+            Unlimited Playtime
+          </h2>
+          <p>265 GEL</p>
+          <p>Sale price: 215 GEL</p>
+          Out of stock
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691057474498-earphone.jpg"
+            alt="Xiaomi Wired in-Ear Earphones with Mic, Ultra Deep Bass & Metal Sound Chamber (Blue)"
+          />
+          <h2>
+            Xiaomi Wired in-Ear Earphones with Mic, Ultra Deep Bass & Metal
+            Sound Chamber (Blue)
+          </h2>
+          <p>5 GEL</p>
+          <p>Sale price: null GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691057718636-headphone5.jpg"
+            alt="boAt Rockerz 370 On Ear Bluetooth Headphones with Upto 12 Hours Playtime, Cozy Padded Earcups and Bluetooth v5.0, with Mic (Buoyant Black)"
+          />
+          <h2>
+            boAt Rockerz 370 On Ear Bluetooth Headphones with Upto 12 Hours
+            Playtime, Cozy Padded Earcups and Bluetooth v5.0, with Mic (Buoyant
+            Black)
+          </h2>
+          <p>12 GEL</p>
+          <p>Sale price: null GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694100438525-51Prg4Smx-L._SL1500_.jpg"
+            alt="Amkette EvoFox Elite Ops Wireless Gamepad for Android TV | Google TV | 8+ Hours of Play Time | Zero Lag Connectivity Upto 12 Feets | USB Extender for TV Included | - Dusk Grey"
+          />
+          <h2>
+            Amkette EvoFox Elite Ops Wireless Gamepad for Android TV | Google TV
+            | 8+ Hours of Play Time | Zero Lag Connectivity Upto 12 Feets | USB
+            Extender for TV Included | - Dusk Grey
+          </h2>
+          <p>18 GEL</p>
+          <p>Sale price: 17 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691074519203-galaxy S22 5G.jpg"
+            alt="Samsung Galaxy S22 5G (Phantom White, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers"
+          />
+          <h2>
+            Samsung Galaxy S22 5G (Phantom White, 8GB RAM, 128GB Storage) with
+            No Cost EMI/Additional Exchange Offers
+          </h2>
+          <p>760 GEL</p>
+          <p>Sale price: 540 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691074776147-galaxy S22 ultra 5G.jpg"
+            alt="Samsung Galaxy S22 Ultra 5G (Burgundy, 12GB, 256GB Storage) "
+          />
+          <h2>Samsung Galaxy S22 Ultra 5G (Burgundy, 12GB, 256GB Storage) </h2>
+          <p>1147 GEL</p>
+          <p>Sale price: 814 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691078463674-poco f1.jpg"
+            alt="Poco by Xiaomi F1 Steel Blue, 6GB RAM, 64GB Storage"
+          />
+          <h2>Poco by Xiaomi F1 Steel Blue, 6GB RAM, 64GB Storage</h2>
+          <p>132 GEL</p>
+          <p>Sale price: null GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691075307831-galaxy M14 5G.jpg"
+            alt="Samsung Galaxy M14 5G (Smoky Teal, 6GB, 128GB Storage)"
+          />
+          <h2>Samsung Galaxy M14 5G (Smoky Teal, 6GB, 128GB Storage)</h2>
+          <p>187 GEL</p>
+          <p>Sale price: 166 GEL</p>
+          Out of stock
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694154515879-819Lw2PE8tL._SL1500_.jpg"
+            alt="Redmi 80 cm (32 inches) HD Ready Smart LED Fire TV L32R8-FVIN (Black)"
+          />
+          <h2>
+            Redmi 80 cm (32 inches) HD Ready Smart LED Fire TV L32R8-FVIN
+            (Black)
+          </h2>
+          <p>138 GEL</p>
+          <p>Sale price: 128 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694154827301-81+JXgPUDLL._SL1500_.jpg"
+            alt="Samsung 108 cm (43 inches) Crystal iSmart 4K Ultra HD Smart LED TV UA43CUE60AKLXL (Black)"
+          />
+          <h2>
+            Samsung 108 cm (43 inches) Crystal iSmart 4K Ultra HD Smart LED TV
+            UA43CUE60AKLXL (Black)
+          </h2>
+          <p>396 GEL</p>
+          <p>Sale price: 360 GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694155239807-81UT46-gwRL._SL1500_.jpg"
+            alt="Mi 125 cm (50 inches) X Series 4K Ultra HD Smart Android LED TV L50M7-A2IN (Black)"
+          />
+          <h2>
+            Mi 125 cm (50 inches) X Series 4K Ultra HD Smart Android LED TV
+            L50M7-A2IN (Black)
+          </h2>
+          <p>384 GEL</p>
+          <p>Sale price: null GEL</p>
+          Available
+        </div>
+        <div className="product">
+          <img
+            src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1694155440745-619--Jabh2L._SL1048_.jpg"
+            alt=" Acer 126 cm (50 inches) V Series 4K Ultra HD Smart QLED Google TV AR50GR2851VQD (Black)"
+          />
+          <h2>
+            {" "}
+            Acer 126 cm (50 inches) V Series 4K Ultra HD Smart QLED Google TV
+            AR50GR2851VQD (Black)
+          </h2>
+          <p>444 GEL</p>
+          <p>Sale price: 422 GEL</p>
+          Available
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App3
